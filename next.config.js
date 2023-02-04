bfec1ed038ffe7284e00390731f22d907acfa319
Will Bailey <wb@fb.com>
2023-02-04 12:34:34 +0900

    works

--- next.config.js
+++ next.config.js
@@ -4,3 +4,13 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports.webpack = (config, { isServer }) => {
+  if (!isServer) {
+    config.resolve.fallback = {
+      fs: false
+    }
+  }
+
+  return config;
+}
